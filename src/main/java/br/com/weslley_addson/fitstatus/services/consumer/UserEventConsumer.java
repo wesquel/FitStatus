d61b84dfc56e2f5320becf3c74344a100aba2bda
Weslley Addson <wesquel@hotmail.com>
2024-10-05 19:10:33 -0300

    Feat: filter created DadosPessoais by AppId

--- src/main/java/br/com/weslley_addson/fitstatus/services/consumer/UserEventConsumer.java
+++ src/main/java/br/com/weslley_addson/fitstatus/services/consumer/UserEventConsumer.java
@@ -1,11 +1,14 @@
 package br.com.weslley_addson.fitstatus.services.consumer;
 
+import br.com.weslley_addson.fitstatus.data.app.AppRequestAuth;
 import br.com.weslley_addson.fitstatus.data.user.UserResponseFitApi;
 import br.com.weslley_addson.fitstatus.exception.BusinessException;
 import br.com.weslley_addson.fitstatus.services.DadosPessoaisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class UserEventConsumer {
@@ -18,10 +21,18 @@ public class UserEventConsumer {
 
     @KafkaListener(topics = "user.created", groupId = "my-group")
     public void receiveMessage(UserResponseFitApi userRequestAuth){
+        Set<AppRequestAuth> appPermissions = userRequestAuth.appsPermission();
+
+        appPermissions.stream()
+                .filter(appRequestAuth -> appRequestAuth.appId() == 1)
+                .forEach(appRequestAuth -> handleUserCreation(userRequestAuth.authUserId()));
+    }
+
+    private void handleUserCreation(Long authUserId) {
         try {
-            dadosPessoaisService.createDadosPessoais(userRequestAuth.authUserId());
+            dadosPessoaisService.createDadosPessoais(authUserId);
         } catch (BusinessException businessException) {
-            // TODO Log error or Resubmit message to queue
+            // TODO: Log the error or resubmit the message to the queue
         }
     }
 }
