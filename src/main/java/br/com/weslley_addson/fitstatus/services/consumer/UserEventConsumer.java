fd00584635c18d46fe84e5f257dffb1676d94deb
Weslley Addson <wesquel@hotmail.com>
2024-10-12 10:52:24 -0300

    refactor: use configurable app ID in UserEventConsumer
    - Replaced hardcoded app ID with dynamic value from configuration (app.id)
    - Used @Value annotation to inject app ID into the consumer
    - Ensured that app ID comparison uses .equals for better type safety

--- src/main/java/br/com/weslley_addson/fitstatus/services/consumer/UserEventConsumer.java
+++ src/main/java/br/com/weslley_addson/fitstatus/services/consumer/UserEventConsumer.java
@@ -5,6 +5,7 @@ import br.com.weslley_addson.fitstatus.data.user.UserResponseFitApi;
 import br.com.weslley_addson.fitstatus.exception.BusinessException;
 import br.com.weslley_addson.fitstatus.services.DadosPessoaisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,9 @@ import java.util.Set;
 
 @Service
 public class UserEventConsumer {
+
+    @Value("${app.id}")
+    private static Long appId;
 
     private final DadosPessoaisService dadosPessoaisService;
 
@@ -24,7 +28,7 @@ public class UserEventConsumer {
         Set<AppRequestAuth> appPermissions = userRequestAuth.appsPermission();
 
         appPermissions.stream()
-                .filter(appRequestAuth -> appRequestAuth.appId() == 1)
+                .filter(appRequestAuth -> appRequestAuth.appId().equals(appId))
                 .forEach(appRequestAuth -> handleUserCreation(userRequestAuth.authUserId()));
     }
 
